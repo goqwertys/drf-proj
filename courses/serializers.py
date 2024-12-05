6a65db48586680fe3c99e9ea7798426ef51fb112
georgii <goqwertys@gmail.com>
2024-12-05 12:57:05 +0400

    nested field lessons added

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,18 +3,22 @@ from rest_framework import serializers
 from courses.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lesson_count(self, instance):
         return instance.lessons.count()
 
     class Meta:
         model = Course
-        fields = ['id', 'name', 'description', 'preview', 'lesson_count']
+        fields = ['id', 'name', 'description', 'preview', 'lesson_count', 'lessons']
 
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+
