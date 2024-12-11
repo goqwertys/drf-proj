45ba5892b9d057179028a750335b4258c83104c7
georgii <goqwertys@gmail.com>
2024-12-11 21:38:03 +0400

    test_lesson_list updated

--- courses/tests.py
+++ courses/tests.py
@@ -32,12 +32,48 @@ class LessonTestCase(APITestCase):
 
     def test_lesson_list(self):
         url = reverse('courses:lesson_list')
-        Lesson.objects.create(course=self.course, title='Lesson 1', owner=self.user)
-        Lesson.objects.create(course=self.course, title='Lesson 2', owner=self.user)
+        lesson_1 = Lesson.objects.create(course=self.course, title='Lesson 1', owner=self.user)
+        lesson_2 = Lesson.objects.create(course=self.course, title='Lesson 2', owner=self.user)
 
         response = self.client.get(url)
+        data = response.json()
+
+        expected_result = {
+            "count": 2,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "id": lesson_1.id,
+                    "title": lesson_1.title,
+                    "preview": None,
+                    "video_url": None,
+                    "course": self.course.id,
+                    "owner": self.user.id
+                },
+                {
+                    "id": lesson_2.id,
+                    "title": lesson_2.title,
+                    "preview": None,
+                    "video_url": None,
+                    "course": self.course.id,
+                    "owner": self.user.id
+                }
+            ]
+        }
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['results']), 2)
+        self.assertEqual(data['count'], expected_result['count'])
+        self.assertEqual(data['next'], expected_result['next'])
+        self.assertEqual(data['previous'], expected_result['previous'])
+
+        for i, lesson_data in enumerate(data['results']):
+            self.assertEqual(lesson_data['id'], expected_result['results'][i]['id'])
+            self.assertEqual(lesson_data['title'], expected_result['results'][i]['title'])
+            self.assertEqual(lesson_data['preview'], expected_result['results'][i]['preview'])
+            self.assertEqual(lesson_data['video_url'], expected_result['results'][i]['video_url'])
+            self.assertEqual(lesson_data['course'], expected_result['results'][i]['course'])
+            self.assertEqual(lesson_data['owner'], expected_result['results'][i]['owner'])
 
     def test_lesson_retrieve(self):
         lesson = Lesson.objects.create(course=self.course, title='Test Lesson', owner=self.user)
