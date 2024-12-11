c31ac5f48e4cc9eede2503f7050ab857cf1c6d13
georgii <goqwertys@gmail.com>
2024-12-11 15:24:25 +0400

    CourseSerializer updated

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from courses.models import Course, Lesson
+from courses.models import Course, Lesson, Subscription
 from courses.validators import NoExternalLinkValidator
 
 
@@ -14,11 +14,31 @@ class LessonSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    id_subscribed = serializers.SerializerMethodField()
 
     def get_lesson_count(self, instance):
         return instance.lessons.count()
 
+    def get_id_subscribed(self, instance):
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            return Subscription.objects.filter(
+                user=user,
+                course=instance,
+                is_active=True
+            ).exists()
+        return False
+
     class Meta:
         model = Course
-        fields = ['id', 'name', 'description', 'preview', 'lesson_count', 'lessons', 'owner']
+        fields = [
+            'id',
+            'name',
+            'description',
+            'preview',
+            'lesson_count',
+            'lessons',
+            'owner',
+            'id_subscribed'
+        ]
         validators = [NoExternalLinkValidator(fields='description')]
