af31a78a7f418e2c73a2972c11d731aab0700104
georgii <goqwertys@gmail.com>
2024-12-11 21:23:24 +0400

    urls by reverse getting added

--- courses/tests.py
+++ courses/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
 from courses.models import Course, Lesson
@@ -23,35 +24,39 @@ class LessonTestCase(APITestCase):
         }
 
     def test_lesson_create(self):
-        response = self.client.post('/courses/lessons/create/', data=self.lesson_data)
+        url = reverse('courses:lesson_create')
+        response = self.client.post(url, data=self.lesson_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Lesson.objects.count(), 1)
         self.assertEqual(Lesson.objects.first().title, 'Test Lesson')
 
     def test_lesson_list(self):
+        url = reverse('courses:lesson_list')
         Lesson.objects.create(course=self.course, title='Lesson 1', owner=self.user)
         Lesson.objects.create(course=self.course, title='Lesson 2', owner=self.user)
 
-        response = self.client.get('/courses/lessons/')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 2)
 
     def test_lesson_retrieve(self):
         lesson = Lesson.objects.create(course=self.course, title='Test Lesson', owner=self.user)
 
-        response = self.client.get(f'/courses/lessons/{lesson.id}/')
+        url = reverse('courses:lesson_detail', args=(lesson.id,))
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Test Lesson')
 
     def test_lesson_update(self):
         lesson = Lesson.objects.create(course=self.course, title='Test Lesson', owner=self.user)
+        url = reverse('courses:lesson_update', args=(lesson.id,))
         updated_data = {
             'course': self.course.id,
             'title': 'Updated Lesson',
             'video_url': 'https://www.youtube.com/watch?v=updated'
         }
 
-        response = self.client.put(f'/courses/lessons/{lesson.id}/update/', data=updated_data)
+        response = self.client.put(url, data=updated_data)
 
         print("Response status code:", response.status_code)
         print("Response data:", response.data)
@@ -61,19 +66,21 @@ class LessonTestCase(APITestCase):
 
     def test_lesson_partial_update(self):
         lesson = Lesson.objects.create(course=self.course, title='Test Lesson', owner=self.user)
+        url = reverse('courses:lesson_update', args=(lesson.id,))
         updated_data = {
             'title': 'Updated Lesson',
             'video_url': 'https://www.youtube.com/watch?v=updated'
         }
 
-        response = self.client.patch(f'/courses/lessons/{lesson.id}/update/', data=updated_data)
+        response = self.client.patch(url, data=updated_data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], 'Updated Lesson')
 
     def test_lesson_delete(self):
         lesson = Lesson.objects.create(course=self.course, title='Test Lesson', owner=self.user)
+        url = reverse('courses:lesson_delete', args=(lesson.id,))
 
-        response = self.client.delete(f'/courses/lessons/{lesson.id}/delete/')
+        response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Lesson.objects.count(), 0)
