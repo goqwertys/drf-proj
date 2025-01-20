2d0c4d96de09e3389744f0ce0ef57598198626da
georgii <goqwertys@gmail.com>
2025-01-21 02:42:41 +0400

    flake8 fixes

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email,  **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
