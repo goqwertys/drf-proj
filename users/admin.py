2b00081bfcad0810b2b4ddb7d93fc0ec8767cb8c
georgii <goqwertys@gmail.com>
2025-01-21 02:41:27 +0400

    ModelAdmin registered

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payment
 
 
 @admin.register(User)
@@ -9,3 +9,28 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('is_staff', 'is_active', 'city',)
     search_fields = ('email', 'phone_number', 'city',)
     ordering = ('id',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'date',
+        'amount',
+        'method',
+        'status',
+        'course',
+        'lesson'
+    )
+    list_filter = (
+        'method',
+        'status',
+        'date'
+    )
+    search_fields = (
+        'user__email',
+        'course__title',
+        'lesson__title',
+        'session_id'
+    )
+    ordering = ('-date',)
